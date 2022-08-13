cf70b52d8c1931408571e53bcf55e06f51dbcbf3
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-14 00:58:34 +0700

    Update data.js

--- sesi-1/data.js
+++ sesi-1/data.js
@@ -2,3 +2,24 @@ const nama = "Fiky-web"
 let usia = 17;
 
 let biodata = document.getElementById('biodata');
+
+function generateBiodata() {
+  let generasi;
+
+  if (usia > 10 && usia < 18) {
+    generasi = "generasi remaja";
+  }
+  else if (usia > 18 && usia < 30) {
+    generasi = "generasi dewasa";
+  }
+  else if (usia >= 30) {
+    generasi = "generasi tua";
+  }
+  else if (usia > 2 && usia < 10) {
+    generasi = "generasi anak anak";
+  }
+  else {
+    generasi = "generasi bayi";
+  }
+  return biodata.innerHTML = generasi;
+}
