6a1cd99b04d09908fb0200fe34d2ca5747a862b2
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2022-08-16 07:05:31 +0700

    Update saklar.js

--- sesi-4/saklar.js
+++ sesi-4/saklar.js
@@ -1,2 +1,4 @@
 function saklar() {
   let toggle1 = document.getElementById("default-toggle1");
+  let toggle2 = document.getElementById("default-toggle2");
+  let toggle3 = document.getElementById("default-toggle3");
